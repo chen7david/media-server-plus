61f17ea1bf00739a766438c8600cd1ee808cb935
chen7david <chen7david@me.com>
2019-12-09 09:10:26 +0800

    update

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -3,17 +3,20 @@ const path = require('path')
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
+        const video = ['video/mp4']
         const images = ['image/jpeg','image/png']
-        if (file.mimetype === 'audio/mp3') {
-            cb(null, 'src/public/uploads/audio')
-          } else if (file.mimetype === 'video/mp4') {
-            cb(null, 'src/public/uploads/video')
+        const audio = ['audio/mp3']
+        
+         if (video.includes(file.mimetype)) {
+              cb(null, 'src/public/uploads/video')
           } else if (images.includes(file.mimetype)) {
-            cb(null, 'src/public/uploads/image')
+              cb(null, 'src/public/uploads/image')
+          } else if (audio.includes(file.mimetype)) {
+              cb(null, 'src/public/uploads/audio')
           } else if (file.mimetype === 'application/pdf') {
-            cb(null, 'src/public/uploads/pdf')
-          }else {
-            cb({ error: 'Mime type not supported' })
+              cb(null, 'src/public/uploads/pdf')
+          } else {
+              cb({ error: 'Mime type not supported' })
           }
     },
     filename: function(req, file, cb) {
